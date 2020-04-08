feeae4e89d490fed3cfb6a5ad4176bf10d924365
alex <uliza.fonar.apteka@gmail.com>
2020-04-08 18:31:28 +0700

    Updates

---
+++ precache-manifest.14b2bf2579e738941b7ff3f7ad5e3e17.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f6f58e2cf2625dbf62147f56a2f71e",
+    "url": "/currency-courses/index.html"
+  },
+  {
+    "revision": "a08c461efda2ab50a366",
+    "url": "/currency-courses/static/css/main.79c4f9e4.chunk.css"
+  },
+  {
+    "revision": "7676183b20f72d1bbf3c",
+    "url": "/currency-courses/static/js/2.4b3cdf03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-courses/static/js/2.4b3cdf03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a08c461efda2ab50a366",
+    "url": "/currency-courses/static/js/main.3f8144be.chunk.js"
+  },
+  {
+    "revision": "8d41e19acd446bcfe510",
+    "url": "/currency-courses/static/js/runtime-main.57b1d97a.js"
+  },
+  {
+    "revision": "a3a6ba2054208914290188251ceb9af5",
+    "url": "/currency-courses/static/media/arrow.a3a6ba20.svg"
+  },
+  {
+    "revision": "41ae3b1f9400c1c3cd13cf038fa9bfb2",
+    "url": "/currency-courses/static/media/close.41ae3b1f.svg"
+  }
+]);
